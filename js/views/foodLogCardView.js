277dfe444eeb635c91644c668b041bf490008030
Sean McBride <spmcbride1201@gmail.com>
2016-07-27 16:45:54 -0400

    feat: foodLog Table displays entries

--- js/views/foodLogCardView.js
+++ js/views/foodLogCardView.js
@@ -9,6 +9,12 @@ app.FoodLogCardView = Backbone.View.extend({
     },
     initialize: function () {
         // this.$input = $('#inputFood');
-    }
+        this.listenTo(app.foodItems, 'add', this.addItem);
+    },
+    addItem: function (foodItem) {
+        var view = new app.FoodLogItemView({ model: foodItem });
+        console.log(view.render().el);
+        $('#foodLogCardTable tbody').append(view.render().el);
+    },
     // render: function () { },
 });
---
+++ js/views/foodLogItemView.js
@@ -0,0 +1,24 @@
+var app = app || {};
+
+app.FoodLogItemView = Backbone.View.extend({
+
+    // Each foodLogItem is diplayed as a row item in the foodLogCard table
+    tagName: 'tr',
+
+    // use the item-template Underscore microtemplate to render as HTML
+    template: _.template($('#foodLogItem-template').html()),
+
+    // When initialized, being listening to the associated model and rerun reder when a change is detected
+    initialize: function () {
+        // this.listenTo(this.model, 'change', this.render);
+        // this.listenTo(this.model, 'destroy', this.remove);
+        // this.listenTo(this.model, 'visible', this.toggleVisible);
+    },
+
+    render: function () {
+        console.log(this.model.attributes);
+        this.$el.html(this.template(this.model.attributes));
+        return this; // this allow for chaining
+    }
+
+});
