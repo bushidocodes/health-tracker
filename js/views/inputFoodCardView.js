900cde9ef76bd1d808a9ba6f906cf1ae3e899e8a
Sean McBride <spmcbride1201@gmail.com>
2016-07-27 20:40:41 -0400

    feat: Initial implementation of Nutritionix search

--- js/views/inputFoodCardView.js
+++ js/views/inputFoodCardView.js
@@ -1,6 +1,9 @@
 // Declare app if not declared
 var app = app || {};
 
+app.nutritionixBuffer = [];
+app.engine;
+
 app.InputFoodCardView = Backbone.View.extend({
     el: '#inputFoodCard',
     events: {
@@ -11,18 +14,32 @@ app.InputFoodCardView = Backbone.View.extend({
         this.$inputFood = $('#inputFood');
         this.$inputAmount = $('#inputAmount');
         this.$inputTime = $('#inputTime');
-        $.get('data/example_collection.json', function (data) {
-            $('#InputFood').blur()
-            $('#inputFood').typeahead({
-                source: data,
-                minLength: 1
-            });
-        }, 'json');
+
+        // app.engine = new Bloodhound({
+        //     local: ['dog', 'pig', 'moose'],
+        //     queryTokenizer: Bloodhound.tokenizers.whitespace,
+        //     datumTokenizer: Bloodhound.tokenizers.whitespace
+        // });
+
+
+
+
+        // $('#inputFood').typeahead({
+        //     source: app.nutritionixBuffer,
+        //     minLength: 1
+        // });
+        // $.get('data/example_collection.json', function (data) {
+        //     $('#InputFood').blur()
+        //     $('#inputFood').typeahead({
+        //         source: data,
+        //         minLength: 1
+        //     });
+        // }, 'json');
     },
     // render: function () { },
     createOnEnter: function (e) {
-        console.log("typeahead running");
-        // console.log(this.$input.val().trim().length);
+        app.nutritionixBuffer = this.searchNutritionix($('#inputFood').val());
+        console.log(app.nutritionixBuffer);
         if (e.which === ENTER_KEY && this.$input.val().trim()) {
             // console.log("calling CreateFoodItem");
             this.createFoodItem(e);
@@ -56,7 +73,23 @@ app.InputFoodCardView = Backbone.View.extend({
         // erase the input field
         this.$inputFood.val('');
         this.$inputAmount.val('');
-        this.$inputTime.val( "Breakfast" );
+        this.$inputTime.val("Breakfast");
     },
 
+    searchNutritionix: function (query) {
+        var results = [];
+        $.getJSON("https://api.nutritionix.com/v1_1/search/" + query + "?results=0:20&fields=item_name,brand_name,item_id,nf_calories&appId=aa511326&appKey=599102964bee7e4e92c3f8a9b4bfcdd4", function (data) {
+
+            // console.log(data.hits);
+            _.each(data.hits, function (hit) {
+                // console.log(hit.fields.brand_name + " " + hit.fields.item_name);
+                // this.name = this.fields.brand_name + " " + this.fields.item_name;
+                // console.log(this.name);
+                results.push(hit.fields.brand_name + " " + hit.fields.item_name);
+            });
+            // return data;
+        });
+        // console.log(results);
+        return results;
+    }
 });
