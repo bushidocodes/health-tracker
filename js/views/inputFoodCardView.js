908be0fdc3b5c4e1cb8e038d60d6576dad4943cf
Sean McBride <spmcbride1201@gmail.com>
2016-07-31 18:51:06 -0400

    feat: Adjust table to make room for delete button

--- js/views/inputFoodCardView.js
+++ js/views/inputFoodCardView.js
@@ -120,7 +120,7 @@ app.InputFoodCardView = Backbone.View.extend({
             itemName: app.inputFood.item_name,
             amount: this.$inputAmount.val().trim(),
             time: this.$inputTime.val().trim(),
-            calories: app.inputFood.nf_calories * this.$inputAmount.val().trim()
+            calories: Math.round(app.inputFood.nf_calories * this.$inputAmount.val().trim())
         };
     },
 
