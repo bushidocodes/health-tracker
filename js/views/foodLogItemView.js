625e452311bdc5293fad7a98ce1158440096a0e2
Sean McBride <spmcbride1201@gmail.com>
2016-07-31 18:45:07 -0400

    feat: Enable foodItem deletion

--- js/views/foodLogItemView.js
+++ js/views/foodLogItemView.js
@@ -7,20 +7,28 @@ app.FoodLogItemView = Backbone.View.extend({
 
     // Each foodLogItem is diplayed as a row item in the foodLogCard table
     tagName: 'tr',
+    events: {
+        'click .destroy': 'clear' //call clear when button.destroy is clicked
+    },
 
     // use the item-template Underscore microtemplate to render as HTML
     template: _.template($('#foodLogItemTemplate').html()),
 
     // When initialized, being listening to the associated model and rerun reder when a change is detected
-    // initialize: function () {
+    initialize: function () {
         // this.listenTo(this.model, 'change', this.render);
-        // this.listenTo(this.model, 'destroy', this.remove);
+        this.listenTo(this.model, 'destroy', this.remove);
         // this.listenTo(this.model, 'visible', this.toggleVisible);
-    // },
+    },
 
     render: function () {
         this.$el.html(this.template(this.model.attributes));
         return this; // this allow for chaining
+    },
+
+    // Delete the foodItem from app.foodItems
+    clear: function () {
+        this.model.destroy();
     }
 
 });
